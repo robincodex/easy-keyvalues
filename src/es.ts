74d30fb23e6fb72008c64cb0428162e341dca2f2
RobinCode <xv.robincode@gmail.com>
2023-04-16 12:29:28 +0800

    Upgrade deps and fixed es.ts

--- src/es.ts
+++ src/es.ts
@@ -3,3 +3,5 @@ import KeyValues3 from './KeyValues3';
 import { KeyValuesComments, KeyValues3Comments } from './Comments';
 
 export { KeyValues, KeyValues3, KeyValuesComments, KeyValues3Comments };
+
+export * from './adapter';
